d13ba391d623e2c8b843ca38fa6cdf4ed6fc9672
Yiannis Koronakis <koronakisy@gmail.com>
2021-05-25 02:04:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -8,3 +8,4 @@ while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established!")
     clientsocket.send(bytes("welcome to the server!", "utf-8"))
+    clientsocket.close()
