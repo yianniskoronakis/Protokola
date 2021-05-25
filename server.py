c4c678a1562a71909d8dddcf313f2f45e8f881b1
Yiannis Koronakis <koronakisy@gmail.com>
2021-05-25 22:05:03 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,20 @@
 import socket
 
-def sendable_data(data):
-    return str(data).rjust(1024, " ").encode("utf-8")
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
-
-num1 = input("Give a number: ")
-num2 = input("Give a number: ")
-
-s.sendall(sendable_data(num1))
-s.sendall(sendable_data(num2))
-
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+while True:
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established!")
+    clientsocket.send(bytes("welcome to the server!", "utf-8"))
+    with clientsocket:
+        num1=clientsocket.recv(1024)
+        num2=clientsocket.recv(1024)
+        num1=int(num1.decode("utf-8").strip())
+        num2=int(num2.decode("utf-8").strip())
+        sum=num1+num2
+        mul=num1*num2
+        sub=num1-num2
+        div=num1/num2
+        clientsocket.sendall(sum)
