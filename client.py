612ec2927489baf7b6db0947e334b6277c74656f
Yiannis Koronakis <koronakisy@gmail.com>
2021-05-25 23:25:35 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,26 @@
 import socket
 
+HOST = "127.0.0.1"
+PORT = 1234
+
+
 def sendable_data(data):
     return str(data).rjust(1024, " ").encode("utf-8")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1234))
 
-num1 = input("Give a number: ")
-num2 = input("Give a number: ")
+if __name__ == "__main__":
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        num1=int(input("Give me a number: "))
+        num2 =int(input("Give me a number: "))
+        s.sendall(sendable_data(num1))
+        s.sendall(sendable_data(num2))
 
-s.sendall(sendable_data(num1))
-s.sendall(sendable_data(num2))
-
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+        data = s.recv(1024)
+        print(f'Received:\n{data.decode("utf-8").strip()}')
+        data = s.recv(1024)
+        print(f'Received:\n{data.decode("utf-8").strip()}')
+        data = s.recv(1024)
+        print(f'Received:\n{data.decode("utf-8").strip()}')
+        data = s.recv(1024)
+        print(f'Received:\n{data.decode("utf-8").strip()}')
