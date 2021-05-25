b157fd157713a70b27f40a3d5f3393f3ea41c731
Yiannis Koronakis <koronakisy@gmail.com>
2021-05-25 15:39:50 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,16 @@
 import socket
 
+def sendable_data(data):
+    return str(data).rjust(1024, " ").encode("utf-8")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
+
+num1 = input("Give a number: ")
+num2 = input("Give a number: ")
+
+s.sendall(sendable_data(num1))
+s.sendall(sendable_data(num2))
 
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
