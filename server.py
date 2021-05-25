7559d53f92073ee99a1b2f6ec68cebab8662ed1d
Yiannis Koronakis <koronakisy@gmail.com>
2021-05-26 00:02:33 +0300

    Update server.py

--- server.py
+++ server.py
@@ -16,12 +16,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("Connected by", addr)
         data = conn.recv(1024)
         data2 = conn.recv(1024)
-        print(data.decode("utf-8").strip())
-        print(data2.decode("utf-8").strip())
+        if (int(data)>=0 and int(data)<=30000) and (int(data2)>=0 and int(data2)<=30000) and int(data2)!=0:
+            print(data.decode("utf-8").strip())
+            print(data2.decode("utf-8").strip())
 
-        a = int(data2.decode("utf-8").strip())
-        b = int(data.decode("utf-8").strip())
-        conn.send(sendable_data(a + b))
-        conn.send(sendable_data(a - b))
-        conn.send(sendable_data(a * b))
-        conn.send(sendable_data(a / b))
+            a = int(data2.decode("utf-8").strip())
+            b = int(data.decode("utf-8").strip())
+            conn.send(sendable_data(a + b))
+            conn.send(sendable_data(a - b))
+            conn.send(sendable_data(a * b))
+            conn.send(sendable_data(a / b))
+        if int(data2) == 0:
+            conn.send(sendable_data("number 2 is 0"))
+        if not(int(data)>=0 and int(data)<=30000):
+            conn.send(sendable_data("number 1 is out of range"))
+        if not(int(data2)>=0 and int(data2)<=30000):
+            conn.send(sendable_data("number 2 is out of range"))
