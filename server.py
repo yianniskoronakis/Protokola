a3c71d99f2eb08b6f67e9a35364845f9ad216a49
Yiannis Koronakis <koronakisy@gmail.com>
2021-05-25 15:41:08 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 import socket
 
-s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
-s.listen(5)
+def sendable_data(data):
+    return str(data).rjust(1024, " ").encode("utf-8")
 
-while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("welcome to the server!", "utf-8"))
-    clientsocket.close()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+num1 = input("Give a number: ")
+num2 = input("Give a number: ")
+
+s.sendall(sendable_data(num1))
+s.sendall(sendable_data(num2))
+
+msg = s.recv(1024)
+print(msg.decode("utf-8"))
